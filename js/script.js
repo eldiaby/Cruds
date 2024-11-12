b2a3a11dfcc994c3a76d1e485e9ced9198d7cf0b
eldiaby <162382547+eldiaby@users.noreply.github.com>
2024-11-12 14:08:06 +0200

    Finishing the project without validation inputs
    Finish the project but still working in the inputs validation.

--- js/script.js
+++ js/script.js
@@ -13,11 +13,32 @@ const productCategoryInput = document.getElementById('category');
 const productTotalPrice = document.getElementById('total-price');
 const submitBtn = document.getElementById('submit');
 const tableBody = document.querySelector('tbody');
+const deleteAllBtn = document.querySelector('.delete-all-btn');
+
+const searchInput = document.getElementById('search-input');
+const searchBtnsContainer = document.querySelector('.search-btns');
+// const searchBtn = document.getElementById('search-btn');
+
 
 const products = JSON.parse(localStorage.getItem('products')) || [];
+let submitBtnMood = 'create';
+let searchMood = 'title';
+let updateProductId;
 
 // =================================================  //
 // Functions
+
+const updateProductsIndexes = function () {
+  products.forEach((product, index) => (product.index = index));
+};
+
+// Update submit UI
+const updateSubmitUi = function () {
+  submitBtn.textContent = submitBtnMood;
+
+  if (submitBtnMood === 'create') productCountInput.style.display = 'block';
+  else productCountInput.style.display = 'none';
+};
 
 // Calc total price
 const calcTotalPrice = function () {
@@ -50,17 +71,23 @@ const createProduct = function () {
     ads: +productAdsInput.value,
     discount: +productDiscountInput.value,
     totalPrice: calcTotalPrice(),
-    count: +productCountInput,
+    count: +productCountInput.value,
     category: productCategoryInput.value,
   };
 
-  products.push(product);
-  updateLocalStorage();
-  setTimeout(clearInputs, 300);
-  displayProduct(product, products.length - 1);
-};
+  if (product.count > 1) {
+    for (let i = 0; i < product.count; i++) products.push(product);
+  } else {
+    products.push(product);
+  }
 
-// Delete product
+  updateProductsIndexes();
+  updateLocalStorage();
+  // setTimeout(clearInputs, 300);
+  clearInputs();
+  readProducts();
+  displayDeleteAllBtn();
+};
 
 // Update localstorage
 const updateLocalStorage = function () {
@@ -83,17 +110,17 @@ const clearInputs = function () {
 };
 
 // Display product
-const displayProduct = function (product, index) {
+const displayProduct = function (product) {
   const html = `
-            <tr data-id="${index}">
-              <!--<td>${index}</td>-->
-              <td>${product.title}</td>
+            <tr data-id="${product.index}">
+              <!--<td>${product.index}</td>-->
+              <td class="capitalize">${product.title}</td>
               <td>${product.price}</td>
               <td>${product.taxes}</td>
               <td>${product.ads}</td>
               <td>${product.discount}</td>
               <td>${product.totalPrice}</td>
-              <td>${product.category}</td>
+              <td class="capitalize">${product.category}</td>
               <td><button type="button" class="update-btn">Update</button></td>
               <td><button type="button" class="delete-btn">Delete</button></td>
             </tr>
@@ -103,24 +130,97 @@ const displayProduct = function (product, index) {
 
 // Read products
 const readProducts = function () {
+  updateProductsIndexes();
   tableBody.innerHTML = '';
-  products.forEach((product, index) => displayProduct(product, index));
+  products.forEach((product) => displayProduct(product));
+  displayDeleteAllBtn();
 };
 
 // Delete product
-const deleteProdect = function (product) {
+const deleteProduct = function (product) {
   const id = product.closest('tr').dataset.id;
   products.splice(id, 1);
+  updateProductsIndexes();
   updateLocalStorage();
   readProducts();
+  displayDeleteAllBtn();
 };
+
 // Update product
+const updateProduct = function () {
+  products[updateProductId].title = productTitleInput.value;
+  products[updateProductId].price = +productPriceInput.value;
+  products[updateProductId].taxes = +productTaxesInput.value;
+  products[updateProductId].ads = +productAdsInput.value;
+  products[updateProductId].discount = +productDiscountInput.value;
+  products[updateProductId].category = productCategoryInput.value;
+
+  updateLocalStorage();
+  setTimeout(clearInputs, 300);
+  readProducts();
+  displayDeleteAllBtn();
+
+  submitBtnMood = 'create';
+  updateSubmitUi();
+};
+
+// Display update product
+const displayUpdateProduct = function (product) {
+  const id = product.closest('tr').dataset.id;
+
+  productTitleInput.value = products[id].title;
+  productPriceInput.value = products[id].price;
+  productTaxesInput.value = products[id].taxes;
+  productAdsInput.value = products[id].ads;
+  productDiscountInput.value = products[id].discount;
+  productCategoryInput.value = products[id].category;
+
+  getTotalPrice();
+  submitBtnMood = 'update';
+  updateSubmitUi();
+  updateProductId = id;
+  scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
 
 // Search product
+const searchProducts = function () {
+  const searchValue = searchInput.value.toLowerCase();
+  let filteredProducts;
+  if (searchMood === 'title') {
+    filteredProducts = products.filter((product) =>
+      product.title.toLowerCase().includes(searchValue)
+    );
+  } else if (searchMood === 'category') {
+    filteredProducts = products.filter((product) =>
+      product.category.toLowerCase().includes(searchValue)
+    );
+  }
+
+  if (filteredProducts.length > 0) {
+    tableBody.innerHTML = '';
+    filteredProducts.forEach((product) => displayProduct(product));
+  } else if (filteredProducts.length === 0 && searchInput.value !== '') {
+    tableBody.innerHTML = '<tr><td>There is no product matched</td></tr>';
+  } else readProducts();
+};
+
+// Display delete all button
+const displayDeleteAllBtn = function () {
+  if (products.length > 0) {
+    deleteAllBtn.classList.add('active');
+    deleteAllBtn.querySelector(
+      '.products-count'
+    ).innerHTML = `(${products.length})`;
+  } else deleteAllBtn.classList.remove('active');
+};
 
 // Init the application
 const init = function () {
   readProducts();
+  displayDeleteAllBtn();
 };
 
 init();
@@ -128,14 +228,40 @@ init();
 // =================================================  //
 // Events
 productPriceInput.addEventListener('keyup', getTotalPrice);
+
 productTaxesInput.addEventListener('keyup', getTotalPrice);
+
 productAdsInput.addEventListener('keyup', getTotalPrice);
+
 productDiscountInput.addEventListener('keyup', getTotalPrice);
-submitBtn.addEventListener('click', createProduct);
+
+submitBtn.addEventListener('click', () =>
+  submitBtnMood === 'create' ? createProduct() : updateProduct()
+);
+
 tableBody.addEventListener('click', function (e) {
   const target = e.target;
 
   if (!target.closest('button')) return;
 
-  if (target.classList.contains('delete-btn')) deleteProdect(target);
+  if (target.classList.contains('delete-btn')) deleteProduct(target);
+  else if (target.classList.contains('update-btn'))
+    displayUpdateProduct(target);
 });
+
+deleteAllBtn.addEventListener('click', function () {
+  products.splice(0);
+  updateLocalStorage();
+  readProducts();
+});
+
+searchBtnsContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('title')) searchMood = 'title';
+  else if (e.target.classList.contains('category')) searchMood = 'category';
+
+  searchInput.placeholder = `Search by ${searchMood}`;
+  searchInput.focus();
+  searchInput.value = '';
+});
+
+searchInput.addEventListener('keyup', searchProducts);
